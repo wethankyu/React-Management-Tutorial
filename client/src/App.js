8b23ebe7588269bf8a28260af7b60683cf7c5e2c
kyu <wethankyu@gmail.com>
2021-02-06 23:39:48 +0900

    Add Get Customers Rest API module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '김규섭',
-    'birthday': '950314',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '이규섭',
-    'birthday': '950314',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '박규섭',
-    'birthday': '950314',
-    'gender': '남자',
-    'job': '대학생'
-  }
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +54,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (< Customer key={c.id} {...c} />); })}
+            {this.state.customers ? this.state.customers.map(c => { return (< Customer key={c.id} {...c} />); }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,37 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello Express!' });
-});
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '김규섭',
+      'birthday': '950314',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '이규섭',
+      'birthday': '950314',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '박규섭',
+      'birthday': '950314',
+      'gender': '남자',
+      'job': '대학생'
+    }
+  ]);
+})
+
+// app.get('/api/hello', (req, res) => {
+//   res.send({ message: 'Hello Express!' });
+// });
 
 app.listen(port, () => console.log(`Listneing on port ${port}`));
